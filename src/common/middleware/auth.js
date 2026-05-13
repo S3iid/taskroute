0d90295d6e46ea06c775d26274dc5af22f7adf97
S3iid <ahhmmeedds86@gmail.com>
2026-05-13 22:20:04 +0300

    wllahi a5er mara

--- src/common/middleware/auth.js
+++ src/common/middleware/auth.js
@@ -2,9 +2,15 @@ import jwt from "jsonwebtoken"
 import { env } from "../../../config/env.service.js"
 
 export const authMiddleware = (req, res, next) => {
-    let { authorization } = req.headers
-    let decoded = jwt.verify(authorization, env.jwtkey)
-
-    req.user = decoded
-    next()
+    try{let { authorization } = req.headers
+     if (!authorization) {
+            return res.status(401).json({ message: "token is required" })
+        }
+   let token = authorization.split(" ")[1]  // extract token after "Bearer "
+        let decoded = jwt.verify(token, env.jwtkey)
+        req.user = decoded
+        next()
+    }catch (error) {
+        res.status(401).json({ message: "invalid token", error: error.message })
+    }
 }
